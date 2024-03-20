4c8703e8b6ce57ecef73928c93844e6871d97259
yordanos-bekele <123406460+yordanos-bekele@users.noreply.github.com>
2024-03-20 09:24:54 +0300

    displaying genres

--- src/component/GenreList.tsx
+++ src/component/GenreList.tsx
@@ -1,3 +1,4 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import { genre } from "../services/hook/useGenre";
 
 interface Props {
@@ -6,11 +7,23 @@ interface Props {
 
 const GenreList = ({ genres }: Props) => {
   return (
-    <ul>
-      {genres.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
-      ))}
-    </ul>
+    <List>
+      <ListItem>
+        {genres.map((genre) => (
+          <HStack key={genre.id} paddingY={"5px"}>
+            <Image
+              key={genre.id}
+              src={genre.image_background}
+              boxSize={"32px"}
+              borderRadius={"4px"}
+            />
+            <Text key={genre.id} fontSize={"lg"}>
+              {genre.name}
+            </Text>
+          </HStack>
+        ))}
+      </ListItem>
+    </List>
   );
 };
 
